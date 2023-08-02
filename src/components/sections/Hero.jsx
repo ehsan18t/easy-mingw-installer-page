b667567a743ecb39c2101fb461a1e0688069c496
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-02 22:40:20 +0600

    fix button positioning issue on hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,7 +4,7 @@ import { FaGithub, FaDownload } from 'react-icons/fa'
 const Hero = () => {
   return (
     <div className="bg-gradient-to-r from-teal-900 to-blue-600 py-20 text-white">
-      <div className="container mx-auto text-center">
+      <div className="mx-auto text-center">
         <h1 className="text-5xl font-bold mb-4 tracking-tight">
           Easy MinGW Installer
         </h1>
@@ -12,22 +12,24 @@ const Hero = () => {
           Simplify your MinGW setup with a seamless two-click installation
           experience.
         </p>
-        <a
-          href="#download-section"
-          className="inline-flex gap-2 bg-gradient-to-r from-blue-600 to-blue-700 text-white py-3 px-8 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg mr-4"
-        >
-          <FaDownload className="text-xl" />
-          Download Now
-        </a>
-        <a
-          href="https://github.com/ehsan18t/easy-mingw-installer"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex gap-2 items-center justify-center bg-gradient-to-r from-gray-700 to-slate-800 text-white py-3 px-6 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
-        >
-          <FaGithub className="text-xl" />
-          View Project on GitHub
-        </a>
+        <div className="flex flex-col md:flex-row gap-2 justify-center mx-8">
+          <a
+            href="#download-section"
+            className="inline-flex gap-2 items-center justify-center bg-gradient-to-r from-blue-600 to-blue-700 text-white py-3 px-8 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
+          >
+            <FaDownload className="text-xl" />
+            Download Now
+          </a>
+          <a
+            href="https://github.com/ehsan18t/easy-mingw-installer"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex gap-2 items-center justify-center bg-gradient-to-r from-gray-700 to-slate-800 text-white py-3 px-6 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
+          >
+            <FaGithub className="text-xl" />
+            View Project on GitHub
+          </a>
+        </div>
       </div>
     </div>
   )
