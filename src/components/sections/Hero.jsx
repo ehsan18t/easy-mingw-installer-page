b19e3c0b10cfb8d51cccda4b575b424a65c419c8
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-03 14:35:37 +0600

    smooth scrolling on download button click

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { FaGithub, FaDownload } from 'react-icons/fa'
 
 const Hero = () => {
+  const jumpTo = (id) => {
+    const relevantElement = document.getElementById(id)
+    relevantElement.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <div className="bg-gradient-to-r from-teal-900 to-blue-600 py-20 text-white">
       <div className="mx-auto text-center">
@@ -13,13 +18,13 @@ const Hero = () => {
           experience.
         </p>
         <div className="flex flex-col md:flex-row gap-2 justify-center mx-8">
-          <a
-            href="#download-section"
+          <button
+            onClick={() => jumpTo('download-section')}
             className="inline-flex gap-2 items-center justify-center bg-gradient-to-r from-blue-600 to-blue-700 text-white py-3 px-8 rounded-full shadow-md transition duration-300 transform hover:scale-105 hover:shadow-lg"
           >
             <FaDownload className="text-xl" />
             Download Now
-          </a>
+          </button>
           <a
             href="https://github.com/ehsan18t/easy-mingw-installer"
             target="_blank"
