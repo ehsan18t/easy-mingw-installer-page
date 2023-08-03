e9aa4538637f6786f4f534fae33fd7282b148f6a
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-03 16:57:14 +0600

    updated few section titles

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -20,7 +20,7 @@ const Features = () => {
     <div className="bg-gray-100 py-12">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-semibold text-center mb-12">
-          Key Features
+          Features
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <Feature
