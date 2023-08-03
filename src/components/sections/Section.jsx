0e3b3f28c14fcdb3079dfc8c2ff50fa497253343
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-03 12:39:40 +0600

    updated section title alignment

--- src/components/sections/Section.jsx
+++ src/components/sections/Section.jsx
@@ -10,8 +10,8 @@ const Section = ({ title, content, backgroundColor, textColor }) => {
     <div
       className={`py-12 px-6 shadow-md border border-gray-300 ${sectionStyle.backgroundColor}`}
     >
-      <div className="container mx-auto">
-        <h2 className="text-3xl mb-2 font-semibold">{title}</h2>
+      <div className="flex flex-col gap-4 mx-auto justify-center items-center">
+        <h2 className="text-3xl font-semibold">{title}</h2>
         <p className={`text-lg text-justify ${sectionStyle.color}`}>
           {content}
         </p>
