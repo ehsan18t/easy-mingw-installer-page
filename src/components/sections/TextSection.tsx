06c24f3ae5dc980f5ce449fa004277f8effe6f69
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-02-04 13:57:35 +0600

    added missing type definition

--- src/components/sections/TextSection.tsx
+++ src/components/sections/TextSection.tsx
@@ -1,4 +1,11 @@
-const TextSection = ({ title, content, backgroundColor, textColor }) => {
+type TextSectionProps = {
+  title: string;
+  content: string;
+  backgroundColor?: string;
+  textColor?: string;
+};
+
+const TextSection = ({ title, content, backgroundColor, textColor }: TextSectionProps) => {
   const sectionStyle = {
     backgroundColor: backgroundColor || 'bg-gray-100',
     color: textColor || 'text-gray-700',
@@ -7,8 +14,8 @@ const TextSection = ({ title, content, backgroundColor, textColor }) => {
   return (
     <div
       className={`border border-gray-300 px-2 py-10  md:px-4 lg:px-6 ${sectionStyle.backgroundColor}`}>
-      <div className="flex flex-col items-center justify-center gap-3">
-        <h2 className="text-3xl font-semibold">{title}</h2>
+      <div className='flex flex-col items-center justify-center gap-3'>
+        <h2 className='text-3xl font-semibold'>{title}</h2>
         <p className={`text-justify text-lg ${sectionStyle.color}`}>{content}</p>
       </div>
     </div>
