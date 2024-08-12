a22a6c35191a21034589d52e24d3ea55556b8294
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-08-12 15:17:00 +0600

    removed everything

--- src/components/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as DownloadSection } from './DownloadSection';
-export { default as Features } from './Features';
-export { default as Footer } from './Footer';
-export { default as Hero } from './Hero';
-export { default as TextSection } from './TextSection';
--- src/types/Release.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Release {
-  description: string;
-  published_at: string;
-  version: string;
-  win64: string;
-  win32: string;
-}
