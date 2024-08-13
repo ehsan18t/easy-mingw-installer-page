c4726ae102a31a6b7ed0a38d08dc8a71d8de6d21
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-08-13 13:13:40 +0600

    updated release card with full info

--- src/components/release/fetch-release.js
+++ src/components/release/fetch-release.js
@@ -1,15 +1,24 @@
-import "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
+import 'https://cdn.jsdelivr.net/npm/marked/marked.min.js'
 
-async function fetchRelease() {
-    await fetch("https://api.github.com/repos/ehsan18t/easy-mingw-installer/releases/latest")
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('release').innerHTML = marked.parse(data.body);
-        })
-        .catch(error => {
-            console.error('Error:', error)
-            return "# Error: " + error.message;
-        });
+const formatDate = (date) => {
+  const releaseDate = new Date(date)
+  return `${releaseDate.getDate()} ${releaseDate.toLocaleString('default', {
+    month: 'long',
+  })} ${releaseDate.getFullYear()}`
 }
 
-await fetchRelease();
+async function fetchRelease () {
+  await fetch('https://api.github.com/repos/ehsan18t/easy-mingw-installer/releases/latest')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('release-version').innerHTML = `Released on ${data.tag_name}`
+      document.getElementById('release').innerHTML = marked.parse(data.body)
+      document.getElementById('release-date').innerHTML = 'Released on ' + formatDate(data.published_at)
+    })
+    .catch(error => {
+      console.error('Error:', error)
+      return '# Error: ' + error.message
+    })
+}
+
+await fetchRelease()
