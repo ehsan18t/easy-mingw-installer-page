bbbbd7ad4d822e31ffd8265f3ffb3d7e366dd351
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-01-17 21:53:00 +0600

    formatted js

--- src/js/fetch-release.ts
+++ src/js/fetch-release.ts
@@ -1,35 +1,53 @@
-import { marked } from 'marked';
+import { marked } from "marked";
 
 const formatDate = (date: string) => {
   const releaseDate = new Date(date);
-  return `${releaseDate.getDate()} ${releaseDate.toLocaleString('default', {
-    month: 'long',
+  return `${releaseDate.getDate()} ${releaseDate.toLocaleString("default", {
+    month: "long",
   })} ${releaseDate.getFullYear()}`;
 };
 
 async function fetchRelease() {
-  await fetch('https://api.github.com/repos/ehsan18t/easy-mingw-installer/releases/latest')
-    .then(response => response.json())
-    .then(async data => {
+  await fetch(
+    "https://api.github.com/repos/ehsan18t/easy-mingw-installer/releases/latest"
+  )
+    .then((response) => response.json())
+    .then(async (data) => {
       // Update Download Links
-      document.getElementById('btn-dl-64')?.setAttribute('href', data.assets.find((asset: any) => asset.name.includes('64-bit')).browser_download_url);
-      document.getElementById('btn-dl-32')?.setAttribute('href', data.assets.find((asset: any) => asset.name.includes('32-bit')).browser_download_url);
+      document
+        .getElementById("btn-dl-64")
+        ?.setAttribute(
+          "href",
+          data.assets.find((asset: any) => asset.name.includes("64-bit"))
+            .browser_download_url
+        );
+      document
+        .getElementById("btn-dl-32")
+        ?.setAttribute(
+          "href",
+          data.assets.find((asset: any) => asset.name.includes("32-bit"))
+            .browser_download_url
+        );
 
       // Update Release Markdown
-      document.getElementById('release-version')!.innerHTML = `Release v${data.tag_name}`;
-      document.getElementById('release')!.innerHTML = await marked.parse(data.body);
-      document.getElementById('release-date')!.innerHTML = 'Released on ' + formatDate(data.published_at);
+      document.getElementById(
+        "release-version"
+      )!.innerHTML = `Release v${data.tag_name}`;
+      document.getElementById("release")!.innerHTML = await marked.parse(
+        data.body
+      );
+      document.getElementById("release-date")!.innerHTML =
+        "Released on " + formatDate(data.published_at);
 
       // Remove Loading
-      document.getElementById('loading')?.remove();
+      document.getElementById("loading")?.remove();
 
       // Visible Download Button & Release Notes
-      document.getElementById('downloads')?.classList.remove('hidden');
-
+      document.getElementById("downloads")?.classList.remove("hidden");
     })
-    .catch(error => {
-      console.error('Error:', error);
-      return '# Error: ' + error.message;
+    .catch((error) => {
+      console.error("Error:", error);
+      return "# Error: " + error.message;
     });
 }
 
