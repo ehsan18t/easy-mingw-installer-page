c3af673b64bf6d240a5362a6d01474a6517e401b
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-05-16 22:20:35 +0600

    cleanups

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-
-import icon from "astro-icon";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), icon()],
-});
