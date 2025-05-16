e8dfce7f7a8037201e8ec531f02c9be513e1ab2d
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-05-16 22:41:31 +0600

    updated codebase with new template

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.me",
+  site: "https://easymingw.vercel.app",
   vite: {
     plugins: [tailwindcss()],
   },
